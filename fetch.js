4d169da990b4ce4c4913d16f3b7d70746847f3f6
Mustafizur Rahman <mustafizur232@gmail.com>
2022-03-11 20:50:07 +0600

    second commit

---
+++ fetch.js
@@ -0,0 +1,11 @@
+console.log(2222);
+console.log(3333);
+console.log(54645);
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
+console.log("hi dipu");
+for (i = 1; i < 1000; i++) {
+    console.log(i);
+}
+console.log("hi roviun");
