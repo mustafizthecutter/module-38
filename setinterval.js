63a0b4f0c7b97dfa90ab864f5efdca2d86e400f0
Mustafizur Rahman <mustafizur232@gmail.com>
2022-03-11 21:29:24 +0600

    third commit

---
+++ setinterval.js
@@ -0,0 +1,14 @@
+console.log("hi pudi");
+console.log("hi mudi");
+// setTimeout(() => {
+//     console.log('hi borinn');
+// }, 4000)
+let seconds = 0;
+const timeID = setInterval(() => {
+    // seconds++
+    console.log(++seconds);
+    if (seconds > 15) {
+        clearInterval(timeID);
+    }
+}, 1000)
+console.log("chiku chiku");
