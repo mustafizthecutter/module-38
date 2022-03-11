c3ca031e5cb448e6c9baed82b3518d41340933d7
Mustafizur Rahman <mustafizur232@gmail.com>
2022-03-11 19:49:52 +0600

    first commit

--- timeout.js
+++ timeout.js
@@ -3,5 +3,11 @@ function callingName() {
     console.log("i am the first");
 }
 console.log("hi robin");
-callingName();
+// setTimeout(callingName, 6000);
+setTimeout(function () {
+    console.log("i am waiting");
+}, 6000)
+setTimeout(() => {
+    console.log("chiku chiku");
+}, 3000)
 console.log("hi tanjil");
