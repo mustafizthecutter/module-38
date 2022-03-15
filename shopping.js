ca0ad6c9aaff05af6f623092bc3ea6eff52b870b
Mustafizur Rahman <mustafizur232@gmail.com>
2022-03-15 18:07:58 +0600

    fourth time

--- shopping.js
+++ shopping.js
@@ -1,10 +1,21 @@
+const displayLocalStorage = () => {
+    const cart = getCart();
+    for (const name in cart) {
+        displayUI(name);
+    }
+}
+
 const addItem = () => {
     const inputField = document.getElementById('product-name');
     const name = inputField.value;
+    if (!name) {
+        return;
+    }
     // display in the ui
     displayUI(name);
     // add to local storage
     addToCart(name);
+    // clear the input field
     inputField.value = '';
 }
 const displayUI = name => {
@@ -26,7 +37,17 @@ const getCart = () => {
 }
 const addToCart = name => {
     const cart = getCart();
-    cart[name] = 1;
+    if (cart[name]) {
+        cart[name] = cart[name] + 1;
+    }
+    else {
+        cart[name] = 1;
+    }
     const cartStringified = JSON.stringify(cart);
     localStorage.setItem('cart', cartStringified);
 }
+const placeOrder = () => {
+    document.getElementById('show-items').textContent = '';
+    localStorage.removeItem('cart');
+}
+displayLocalStorage();
